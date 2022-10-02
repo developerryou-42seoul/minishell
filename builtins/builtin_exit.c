950edd44a62f4519ad3410e5eae5bfa71f232791
Jongmun Lee <jonglee@c9r5s1.42seoul.kr>
2022-10-02 20:32:34 +0900

    aa

---
+++ builtins/builtin_exit.c
@@ -0,0 +1,11 @@
+#include "minishell.h"
+
+int		builtin_exit(t_block *block)
+{
+	//if (block->argc == )
+	//else if (block->argc == )
+	exit(0);
+	return (0);
+}
+
+//https://tldp.org/LDP/abs/html/exitcodes.html
