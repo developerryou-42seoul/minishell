9148270e97d7503447dbac01489a7acc768b715c
namzisun <namzisun@gmail.com>
2022-10-04 16:16:23 +0900

    update unset : use dic

--- builtins/builtin_unset.c
+++ builtins/builtin_unset.c
@@ -12,31 +12,22 @@
 
 #include "minishell.h"
 
-void	set_key(t_block *block)
-{
-	t_list	*cur;
-
-	cur = block->argv->next;
-	while (cur)
-	{
-		cur->content = mini_join_no_free(cur->content, '=');
-		cur = cur->next;
-	}
-}
-
 void	ft_unset(t_list *cur, t_list *prev, t_list *env)
 {
+	t_dic	*dic;
+
 	while (cur)
 	{
-		if (!ft_strncmp(cur->content, env->content, ft_strlen(env->content)))
+		dic = cur->content;
+		if (!ft_strncmp(dic->key, env->content, ft_strlen(env->content)))
 		{
 			if (prev)
 			{
 				prev->next = cur->next;
 				cur->next = NULL;
 			}
+			free(dic);
 			free(cur);
-			break ;
 		}
 		prev = cur;
 		cur = cur->next;
@@ -51,7 +42,6 @@ int	builtin_unset(t_block *block)
 	int		i;
 
 	i = 1;
-	set_key(block);
 	env = block->argv->next;
 	while (i < block->argc)
 	{
