888dff26c901b7d3a1fa1158261e85779081c93c
Jongmun Lee <jonglee@c10r5s2.42seoul.kr>
2022-10-10 18:08:02 +0900

    d

--- utils/error2.c
+++ utils/error2.c
@@ -6,7 +6,7 @@
 /*   By: jnam <jnam@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 16:13:33 by jnam              #+#    #+#             */
-/*   Updated: 2022/10/06 16:13:34 by jnam             ###   ########.fr       */
+/*   Updated: 2022/10/10 18:07:55 by jonglee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	error_env(char *str, char *arg)
 {
-	ft_putstr_fd("mini: env:", 2);
+	ft_putstr_fd("mini: env :", 2);
 	ft_putstr_fd(arg, 2);
 	ft_putstr_fd(": ", 2);
 	ft_putstr_fd(str, 2);
